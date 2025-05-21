3bb92a334cda6009c7a6716ce9c726f30fc2cf79
car3720 <157552024+car3720@users.noreply.github.com>
2025-05-20 23:57:24 -0600

    Add files via upload

---
+++ PWM0/PWM0.h
@@ -0,0 +1,23 @@
+//******************************************************************************
+// Universidad Del Valle De Guatemala
+// IE2023: Programación de Microcontroladores
+// Created: 29/04/2025 15:02:40
+// Author : Diego Cardona
+// Proyecto: Brazo Robótico
+// Hardware: Atmega238PB
+// Creado: 29/04/2025
+//Última modificación: 29/04/2025
+//******************************************************************************
+
+
+#ifndef PWM0_H_
+#define PWM0_H_
+#include <stdint.h>
+#include <avr/io.h>
+
+void init_PWM0A(int orientacion,int modo,int preescaler);
+void init_PWM0B(int orientacion);
+void duty_cycleA(int duty);
+void duty_cycleB(int duty);
+
+#endif /* PMW0_H_ */
