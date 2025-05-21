0d68c5f22f3be5c04a22575eb91582ae639e2ac1
car3720 <157552024+car3720@users.noreply.github.com>
2025-05-20 23:57:57 -0600

    Add files via upload

---
+++ PWM2/PWM2.c
@@ -0,0 +1,79 @@
+//******************************************************************************
+// Universidad Del Valle De Guatemala
+// IE2023: Programación de Microcontroladores
+// Created: 29/04/2025 15:02:40
+// Author : Diego Cardona
+// Proyecto: Brazo Robótico
+// Hardware: Atmega238PB
+// Creado: 29/04/2025
+//Última modificación: 29/04/2025
+//******************************************************************************
+
+
+#include "PWM2.h" 
+
+void init_PWM2A(int orientacion,int modo,int preescaler){
+	//Puerto de salida
+	DDRB |= (1<<DDB3);
+	TCCR2A=0;
+	TCCR2B=0;
+	
+	//Definimos la orientación en invertido o no invertido
+	if (orientacion==0){
+		//No invertido
+		TCCR2A |= (1<<COM2A1);
+		}else{
+		//invertido
+		TCCR2A |= (1<<COM2A1) | (1<<COM2A0);
+	}
+	//Definimos el modo de operación FAST o Phase Correct
+	if (modo==0){
+		//FAST
+		TCCR2A |= (1<<WGM20) | (1<<WGM21);
+		} else{
+		//Phase Correct
+		TCCR2A |= (1<<WGM20);
+	}
+	//Definimos el preescaler
+	switch(preescaler){
+		case 0:
+		TCCR2B |= (1<<CS20);
+		break;
+		case 8:
+		TCCR2B |= (1<<CS21);
+		break;
+		case 64:
+		TCCR2B |= (1<<CS21)|(1<<CS20);
+		break;
+		case 256:
+		TCCR2B |= (1<<CS21)|(1<<CS22);
+		break;
+		case 1024:
+		TCCR2B |= (1<<CS21)|(1<<CS22)|(1<<CS20);
+		break;
+		
+	}
+	
+}
+
+void init_PWM2B(int orientacion){
+	//Puerto de salida
+	DDRD |= (1<<DDD3);
+	//Definimos la orientación en invertido o no invertido
+	if (orientacion==0){
+		//No invertido
+		TCCR2A |= (1<<COM2B1);
+		}else{
+		//invertido
+		TCCR2A |= (1<<COM2B1) | (1<<COM2B0);
+	}
+	
+}
+
+void duty_cycle2A(int duty){
+	OCR2A = duty;
+}
+
+void duty_cycle2B(int duty){
+	OCR2B = duty;
+}
---
+++ PWM2/PWM2.h
@@ -0,0 +1,22 @@
+//******************************************************************************
+// Universidad Del Valle De Guatemala
+// IE2023: Programación de Microcontroladores
+// Created: 29/04/2025 15:02:40
+// Author : Diego Cardona
+// Proyecto: Brazo Robótico
+// Hardware: Atmega238PB
+// Creado: 29/04/2025
+//Última modificación: 29/04/2025
+//******************************************************************************
+
+#ifndef PWM2_H_
+#define PWM2_H_
+#include <stdint.h>
+#include <avr/io.h>
+
+void init_PWM2A(int orientacion,int modo,int preescaler);
+void init_PWM2B(int orientacion);
+void duty_cycle2A(int duty);
+void duty_cycle2B(int duty);
+
+#endif /* PWM2_H_ */
